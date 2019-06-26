ac480b9498b8c29cce6af0b884ff2e15005d5f6c
KrunalSParmar <parmarkrunal300@yahoo.com>
2019-06-26 10:58:30 -0400

    3 files modified, 4 added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items = [];
+
+  //add item to cart.
+  addToCart(product){
+    this.items.push(product);
+  }
+
+  //return cart items
+  getItems(){
+    return this.items;
+  }
+
+  //Empty the cart.
+  ClearCart(){
+    this.items = [];
+    return this.items;
+  }
+
+  //Calculate shipping cost.
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json')
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+}
